dd309183c3ed70873cc5b7d570fb985783ca55a5
OwenLaRosa <owen.larosa@outlook.com>
2016-06-19 16:30:23 -0400

    Abstract forecast fragment to new file, implement AsyncTask

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,138 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Fragment that displays 7 day weather forecast
+ */
+public class ForecastFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
+    ListView forecastListView;
+    private String apiKey = "83ca909b4cd38c2adeee23c2b697ca65";
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecasts = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7",
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecasts));
+        mForecastAdapter = new ArrayAdapter<String>(
+                // fragment's current context or parent activity
+                getActivity(),
+                // list item layout
+                R.layout.list_item_forecast,
+                // text view within the list item
+                R.id.list_item_forecast_textview,
+                // array of data to populate the list view
+                weekForecast
+        );
+
+        forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        protected Void doInBackground(Void... params) {
+            // class for connecting to the network
+            HttpURLConnection urlConnection = null;
+            // class for reading the returned data
+            BufferedReader reader = null;
+            // string to hold the returned data in JSON format
+            String forecastJsonStr = null;
+
+            try {
+                // url to be used in the GET request
+                URL url = new URL("https://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                // open connection to the URL
+                urlConnection = (HttpURLConnection) url.openConnection();
+                // specify the type of request
+                urlConnection.setRequestMethod("GET");
+                // connect to the server
+                urlConnection.connect();
+                // get the input stream
+                InputStream inputStream = urlConnection.getInputStream();
+                // read the stream into a string
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                // initialize the previously defined buffer reader
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                // each line of returned JSON data
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // append to the buffer and add a newline for readability
+                    buffer.append(line + "\n");
+                }
+                // check if the stream is empty
+                if (buffer.length() == 0) {
+                    // if so, then there's no need for parsing
+                    return null;
+                }
+                // convert the buffer's contents to string format
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                // log the error
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    // disconnect from the server
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream: ", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
