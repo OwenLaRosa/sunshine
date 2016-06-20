760ce4da9e4c4ccb2e5b5c571b41c85c5ce9219d
OwenLaRosa <owen.larosa@outlook.com>
2016-06-20 12:04:33 -0400

    Remove verbose logs

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -207,9 +207,6 @@ public class ForecastFragment extends Fragment {
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
 
-            for (String s : resultStrs) {
-                Log.v(LOG_TAG, "Forecast entry: " + s);
-            }
             return resultStrs;
 
         }
@@ -253,7 +250,6 @@ public class ForecastFragment extends Fragment {
 
                 // url to be used in the GET request
                 URL url = new URL(builtUri.toString());
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
                 // open connection to the URL
                 urlConnection = (HttpURLConnection) url.openConnection();
                 // specify the type of request
@@ -282,8 +278,6 @@ public class ForecastFragment extends Fragment {
                 }
                 // convert the buffer's contents to string format
                 forecastJsonStr = buffer.toString();
-                // log the data to verify that it's correct
-                Log.v(LOG_TAG, "Forecast JSON string: " + forecastJsonStr);
             } catch (IOException e) {
                 // log the error
                 Log.e(LOG_TAG, "Error ", e);
