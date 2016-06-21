05ee59c916d33219530376b692a265a206587474
OwenLaRosa <owen.larosa@outlook.com>
2016-06-21 10:58:18 -0400

    Add detail activity, present it when tapping a forecast

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -85,7 +87,15 @@ public class ForecastFragment extends Fragment {
 
         forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(mForecastAdapter);
-
+        forecastListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String forecast = mForecastAdapter.getItem(i);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailIntent);
+            }
+        });
         return rootView;
     }
 
