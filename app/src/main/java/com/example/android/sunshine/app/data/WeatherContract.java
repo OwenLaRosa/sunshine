a3ade1ef6af228778912f7e55960907df9aecada
OwenLaRosa <owen.larosa@outlook.com>
2016-07-07 11:40:07 -0400

    feat: Add location entry column names

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,15 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        // zip code used to query open weather map API
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // human-readable name of the city
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // exact coordinates to be shown on the map
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
     }
 
     /* Inner class that defines the table contents of the weather table */
